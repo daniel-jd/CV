4693477409c9b326deb47fa8649c4a81cb61682d
Daniel_Y <daniel.yarmak@gmail.com>
2021-05-29 20:08:48 +0300

    Initial Commit

---
+++ CV/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CV
+//
+//  Created by Daniel Yamrak on 29.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
